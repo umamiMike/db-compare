89e715be5b3520a0bcdb3ef66abe854b2b1d020f
Michael Wilding <michael.w.chastain@gmail.com>
2020-06-24 15:27:23 -0700

    comments out for no errors WIP

--- db-compare-react-client/src/App.tsx
+++ db-compare-react-client/src/App.tsx
@@ -1,73 +1,61 @@
 import React from "react";
+/* import TextArea from "./TextArea"; */
 import "./App.css";
 
-const App = (props: any) => {
-  const config = {
-    endpoint: "http://localhost:9099",
-  };
+export default function App(props: any) {
+  /* * const [state, setState] = useState([]); */
+  /* const [query, setQuery] = useState(); */
 
-  let querystring = " ";
-  let querypos = 0;
-  let dbname = "new";
-  let columns = [];
-  let rowdata = [];
-  let dbs = ["ae1", "New", "Test"];
-  let previousqueries = [];
+  /* const config = { */
+  /*   endpoint: "http://localhost:9099", */
+  /* }; */
 
-  const saveCursor = (event: any) => {
-    event.preventDefault();
-    let querypos = event.target.selectionStart;
-    let se = event.target.selectionEnd;
-    return "";
-  };
+  /* // TODO: 
+     define 
+     convert this data from hard coded vars to interface, and props 
+    do I care about  intial working or just doing a redesign based on functionality?
+  */
 
-  const insertIntoQuery = (newVal: any, event: any) => {
-    let astr = querystring.substr(0, querypos);
-    let cstr = querystring.substr(querypos);
-    querystring = astr + " " + newVal + cstr;
-  };
   interface Payload {
     query: string;
     db: string;
   }
 
-  const makeQuery = (e: Event, payload: Payload) => {
-    let rowdata = [];
-    QueryForm(payload);
-  };
+  /* const makeQuery = (e: any) => { */
+  /*   e.preventDefault(); */
+  /*   /1* return QueryForm(payload); *1/ */
+  /* }; */
 
-  const QueryForm = (payload: Payload) => {
-    fetch(config.endpoint, {
-      method: "POST",
-      body: JSON.stringify(payload),
-    })
-      .then(function (response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response;
-      })
-      .then(function (response) {
-        return response.json();
-      })
-      .then((data) => {
-        rowdata = data;
-        return data;
-      })
-      .then((data) => {
-        columns = Object.keys(data.Data[0]);
-        var parsedArray = data.Data.map(function (row: object) {
-          return Object.values(row);
-        });
-        rowdata = parsedArray;
-      })
-      .catch(function (error) {});
-  };
+  /* const QueryForm = (payload: Payload) => { */
+  /*   let rowdata = []; */
+  /*   let columns = []; */
 
-  const replaceQueryString = (newVal: any, event: Event) => {
-    let querystring = newVal;
-    return querystring;
-  };
+  /*   fetch(config.endpoint, { */
+  /*     method: "POST", */
+  /*     body: JSON.stringify(payload), */
+  /*   }) */
+  /*     .then(function (response) { */
+  /*       if (!response.ok) { */
+  /*         throw Error(response.statusText); */
+  /*       } */
+  /*       return response; */
+  /*     }) */
+  /*     .then(function (response) { */
+  /*       return response.json(); */
+  /*     }) */
+  /*     .then((data) => { */
+  /*       rowdata = data; */
+  /*       return data; */
+  /*     }) */
+  /*     .then((data) => { */
+  /*       columns = Object.keys(data.Data[0]); */
+  /*       var parsedArray = data.Data.map(function (row: object) { */
+  /*         return Object.values(row); */
+  /*       }); */
+  /*       rowdata = parsedArray; */
+  /*     }) */
+  /*     .catch(function (error) {}); */
+  /* }; */
 
   const uploadForm = () => {
     return (
@@ -83,77 +71,79 @@ const App = (props: any) => {
       </form>
     );
   };
+
   return (
     <div className="App">
       {uploadForm()}
       <div className="Hello">
-        {/* <div id="replacequery_cont"> */}
-        {/*   <button className="replacequery"></button> */}
-        {/* </div> */}
-        <h1> put message here</h1>
-        <div id="someform" className="form">
+        <div id="queryForm" className="form">
           <div className="tab-content">
-            <div id="signup">
-              <form>
-                <div className="top-row">
-                  <div className="field-wrap">
-                    <label>
-                      Database <span className="req">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      v-model="dbname"
-                      placeholder="ae1"
-                    ></input>
-                  </div>
-                  <div className="field-wrap">
-                    <label>
-                      Query String<span className="req">*</span>
-                    </label>
-                    <textarea
-                      className="queryinput"
-                      cols={40}
-                      rows={5}
-                      onClick={() => saveCursor}
-                      placeholder="Describe Users"
-                    >
-                      show tables
-                    </textarea>
-                    {/* <button type="button" onClick={makeQuery}> */}
-                    {/*   load data */}
-                    {/* </button> */}
-                  </div>
-                </div>
-              </form>
-            </div>
+            {/* <div id="signup"> */}
+            {/*   <form> */}
+            {/*     <div className="top-row"> */}
+            {/*       <div className="field-wrap"> */}
+            {/*         <label> */}
+            {/*           Database <span className="req">*</span> */}
+            {/*         </label> */}
+            {/*         <input */}
+            {/*           type="text" */}
+            {/*           v-model="dbname" */}
+            {/*           placeholder="ae1" */}
+            {/*         ></input> */}
+            {/*       </div> */}
+            {/*       <div className="field-wrap"> */}
+            {/*         <label> */}
+            {/*           Query String<span className="req">*</span> */}
+            {/*           :q */}
+            {/*         </label> */}
+            {/*         <textarea */}
+            {/*           className="queryinput" */}
+            {/*           cols={40} */}
+            {/*           rows={5} */}
+            {/*           placeholder="Describe Users" */}
+            {/*         > */}
+            {/*           TextArea.tsx show tables */}
+            {/*         </textarea> */}
+
+            {/*         <button type="button" onClick={makeQuery}></button> */}
+            {/*       </div> */}
+            {/*     </div> */}
+            {/*   </form> */}
+            {/* </div> */}
           </div>
         </div>
       </div>
-      <div className="tablecontainer">
-        <table>
-          <tr className="header">
-            {/* <th v-for="header in columns"> */}
-            <th>header a</th>
-            <th>header b</th>
-            <th>header c</th>
-          </tr>
-          <tr>
-            {/* <td v-for="fielddata in row"> */}
-            <td>
-              datapoint 1{/* <h2 onClick={insertIntoQuery}> fielddata </h2> */}
-            </td>
-            <td>
-              datapoint 2{/* <h2 onClick={insertIntoQuery}> fielddata </h2> */}
-            </td>
 
-            <td>
-              datapoint 3{/* <h2 onClick={insertIntoQuery}> fielddata </h2> */}
-            </td>
-          </tr>
-        </table>
-      </div>
+      {/* <TextArea name="testo" onChange={makeQuery} /> */}
+      {/* TODO: create tabular data
+          headers are the keys 
+          <TabularData/>
+
+        */}
+      {/* <div className="tablecontainer"> */}
+      {/*   <table> */}
+      {/*     <tr className="header"> */}
+      {/*       {/1* <th v-for="header in columns"> *1/} */}
+      {/*       <th>header a</th> */}
+      {/*       <th>header b</th> */}
+      {/*       <th>header c</th> */}
+      {/*     </tr> */}
+      {/*     <tr> */}
+      {/*       {/1* <td v-for="fielddata in row"> *1/} */}
+      {/*       {/1* TODO: DataPointRows list *1/} */}
+      {/*       <td> */}
+      {/*         datapoint 1{/1* <h2 onClick={insertIntoQuery}> fielddata </h2> *1/} */}
+      {/*       </td> */}
+      {/*       <td> */}
+      {/*         datapoint 2{/1* <h2 onClick={insertIntoQuery}> fielddata </h2> *1/} */}
+      {/*       </td> */}
+
+      {/*       <td> */}
+      {/*         datapoint 3{/1* <h2 onClick={insertIntoQuery}> fielddata </h2> *1/} */}
+      {/*       </td> */}
+      {/*     </tr> */}
+      {/*   </table> */}
+      {/* </div> */}
     </div>
   );
-};
-
-export default App;
+}
