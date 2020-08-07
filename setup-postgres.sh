43a4cd8180621569241aa0b7dbef1f20da8e4972
Michael Wilding <michael.w.chastain@gmail.com>
2020-08-07 01:53:14 +0000

    simplifies, adds comments to the setup-postgres.sh

--- setup-postgres.sh
+++ setup-postgres.sh
@@ -1,23 +1,24 @@
-# init the db  
-
+# make the data directory if needed 
+mkdir -p postgres
+# necessary env vars
 export PGDATA="$(pwd)/postgres"
 # Place Postgres' Unix socket inside the data directory
 export PGHOST="$PGDATA"
 
-chmod +x setup-postgres.sh
+# initialize the db
+initdb
 
+# add the appropriate configuration file
 cat > "$PGDATA/postgresql.conf" <<EOF
 listen_addresses = 'localhost'
 port = 5432
 unix_socket_directories = '$PGHOST'
 EOF
 
-# echo "init the db? y " 
-# read initdb
-# if [  $initdb = "y" ]; then
-  initdb
-  # ...configure it to listen only on the Unix socket, and...
-  echo "CREATE DATABASE postgres;" | postgres --single -E postgres
-  echo "CREATE USER postgres --createdb;" | postgres --single -E postgres
-# fi
+# create the base db
+echo "CREATE DATABASE postgres;" | postgres --single -E postgres
+# create the user
+echo "CREATE USER postgres --createdb;" | postgres --single -E postgres
+
+# start the instance
 postgres
