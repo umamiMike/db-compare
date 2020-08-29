8f691598f299c13fc26771422cf75ea205336ca6
Michael Wilding <michael.w.chastain@gmail.com>
2020-08-28 17:49:55 -0700

    moves process starting out of nix mkShell

---
+++ start-processes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Place the data directory inside the project directory
+      # function cleanup {
+      # echo "exiting the shell and killing postgres" 
+      # killall postgres
+      # }
+
+
+root="$(pwd)"
+
+tmux new-session -d -s db-compare-dev
+
+tmux new-window -t db-compare-dev -n umbrella
+
+tmux new-window -t db-compare-dev -n server
+# tmux send-keys 'cd ./db-compare-server/ && go build && ./db-compare-server' 'C-m'
+
+tmux new-window -t db-compare-dev -n react-client
+# tmux send-keys  'cd react-client &&  npm run start' 'C-m'
+
+tmux new-window -t db-compare-dev -n postgres-db
+# tmux send-keys  'source setup-postgres.sh' 'C-m'
+
+      # tmux attach -t db-compare-dev
