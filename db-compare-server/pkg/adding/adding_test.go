62d1cb6a58688736fba76334c9dd7dedb24cf1e3
Michael Wilding <michael.w.chastain@gmail.com>
2020-09-03 09:19:09 -0700

    mvp of datsource service passing basic test

---
+++ db-compare-server/pkg/adding/adding_test.go
@@ -0,0 +1,43 @@
+package adding
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAddDatasource(t *testing.T) {
+	ds := Datasource{
+		Username: "mike",
+		Hostname: "localhost",
+		Password: "postgres",
+		DbName:   "test",
+	}
+	d2 := Datasource{
+		Username: "mike",
+		Hostname: "localfost",
+		Password: "postgres",
+		DbName:   "test",
+	}
+
+	mR := new(mockStorage)
+	s := NewService(mR)
+	err := s.AddDatasource(ds, d2)
+	storedDatasources := mR.GetAllDatasources()
+
+	assert.Len(t, storedDatasources, 2)
+	require.NoError(t, err)
+}
+
+type mockStorage struct {
+	datasources []Datasource
+}
+
+func (m *mockStorage) AddDatasource(ds Datasource) error {
+	m.datasources = append(m.datasources, ds)
+	return nil
+}
+
+func (m *mockStorage) GetAllDatasources() []Datasource {
+	return m.datasources
+}
