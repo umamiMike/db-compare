2c1d71e7280361a5ce832ba3b8cbb04b100bd363
Michael Wilding <michael.w.chastain@gmail.com>
2020-09-08 08:18:40 -0700

    adds fastshutdown of postgres server into startprocess for restartability

--- start-processes.sh
+++ start-processes.sh
@@ -8,17 +8,19 @@
 # new window 
 #    -c  is the starting directory
 root="$(pwd)"
-
+kill -INT `head -1 $(pwd)/postgres/postmaster.pid`
 tmux kill-session -t db-compare-dev
+sleep 2
 tmux new-session -d -s db-compare-dev 
 
-tmux new-window -t db-compare-dev:2 -n server -c $root
-tmux send-keys -t db-compare-dev:2 'cd db-compare-server/ && ./dev-build.sh' Enter
+tmux new-window -t db-compare-dev:0 -n server -c $root
+tmux send-keys -t db-compare-dev:0 'cd db-compare-server/ && ./dev-build.sh' Enter
 
-tmux new-window -t db-compare-dev:3 -n react-client -c $root
-tmux send-keys -t db-compare-dev:3 'cd react-client && npm run start' Enter 
+tmux new-window -t db-compare-dev:1 -n react-client -c $root
+tmux send-keys -t db-compare-dev:1 'cd react-client && npm run start' Enter 
 
-tmux new-window -t db-compare-dev:4 -n postgres-db -c $root
-tmux send-keys  -t db-compare-dev:4 "./setup-postgres.sh" Enter 
+tmux new-window -t db-compare-dev:2 -n postgres-db -c $root
+tmux send-keys  -t db-compare-dev:2 "./setup-postgres.sh" Enter 
 
-# tmux attach -t db-compare-dev
+tmux attach -t db-compare-dev
+echo "processes started"
