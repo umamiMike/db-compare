624766c16de38f95494daa51d2b3dde9e2527d77
Michael Wilding <heymichaelwilding@gmail.com>
2021-12-11 19:27:46 -0800

    adds experimental section taken from sandbox code

---
+++ experiments/go-csv-json/README.md
@@ -0,0 +1,6 @@
+ csv-to-json
+ =========================
+This is a Work in progress!!!
+
+ a little lib and cli tool to convert csv data into json objects
+
---
+++ experiments/go-csv-json/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/umamimike/go-csv-json/config"
+	"github.com/umamimike/go-csv-json/utils"
+	"github.com/umamimike/go-csv-json/validate"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	infile := os.Args[1]
+	delimiter := os.Args[2]
+	processCsv(infile, delimiter)
+}
+
+func processCsv(csvfile string, delimiter string) {
+
+	f, err := os.Open(csvfile)
+	if err != nil {
+		log.Panic(err)
+	}
+	type keyval []interface{}
+	reader := bufio.NewReader(f)
+	heads, _, _ := reader.ReadLine()
+	headSlice := strings.Split(string(heads), delimiter)
+
+	scanner := csv.NewScanner(f)
+	var parsedCsv keyval
+	for scanner.Scan() {
+		recordMap := make(map[string]string)
+		if err := scanner.Error(); err != nil {
+			log.Panic(err)
+		} else {
+			for key, record := range scanner.Record() {
+
+				fmt.Println("the key in the scanner record range is: ", key, ", and the record is: ", record)
+				fmt.Println("the key in the headslice is", headSlice[key])
+				recordMap[headSlice[key]] = record
+			}
+		}
+
+		parsedCsv = append(parsedCsv, recordMap)
+	}
+	j, _ := json.MarshalIndent(parsedCsv, "", "    ")
+	ioutil.WriteFile("output.json", j, 0644)
+
+}
