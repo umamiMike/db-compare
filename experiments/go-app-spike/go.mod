ed81bdbe1f106e8565852f97bc0020697e54262c
Michael W.C. Wilding <michael.w.chastain@gmail.com>
2022-09-25 15:35:12 -0700

    adds go app spike
    https://go-app.dev/getting-started\#build-the-client
    `GOARCH=wasm GOOS=js go build -o web/app.wasm && go build -o foo && ./foo`

---
+++ experiments/go-app-spike/go.mod
@@ -0,0 +1,8 @@
+module example.com/m/v2
+
+go 1.18
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/maxence-charriere/go-app/v9 v9.6.4 // indirect
+)
---
+++ experiments/go-app-spike/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
+
+type hello struct {
+	app.Compo
+
+	name string
+	bar  string
+}
+
+func (h *hello) Render() app.UI {
+	h.bar = "i am the baz"
+	return app.Div().Body(
+		app.H1().Body(app.Text(h.bar)),
+		app.H1().Body(
+			app.Text("Hello, "),
+			app.If(h.name != "",
+				app.Text(h.name),
+			).Else(
+				app.Text("World!"),
+			),
+		),
+		app.P().Body(
+			app.Input().
+				Type("text").
+				Value(h.name).
+				Placeholder("What is your name?").
+				AutoFocus(true).
+				OnChange(h.ValueTo(&h.name)),
+		),
+	)
+}
+
+func main() {
+	// Components routing:
+	app.Route("/", &hello{})
+	app.Route("/hello", &hello{})
+	app.RunWhenOnBrowser()
+
+	// HTTP routing:
+	http.Handle("/", &app.Handler{
+		Name:        "Hello",
+		Description: "An Hello World! example",
+	})
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
